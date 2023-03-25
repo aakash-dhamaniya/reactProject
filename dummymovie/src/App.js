60934b4d47f54966fb80b884fca10dc8fa39877b
aakash-dhamaniya <aakashdhamaniya.dev@gmail.com>
2023-03-26 02:42:13 +0530

    added ad movie component and console log all movies from input

---
+++ dummymovie/src/App.js
@@ -0,0 +1,81 @@
+import React, { useCallback, useEffect, useState } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+let retrying = null;
+var arr = [];
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isloader, setisLoader] = useState(false);
+  const [error, setError] = useState(null);
+  const [isCancel, setisCancel] = useState(true);
+  const fetchMoviesHandler = useCallback(async () => {
+    setError(null);
+    setisLoader(true);
+    setMovies([]);
+    try {
+      const res = await fetch("https://swapi.dev/api/films/");
+      if (!res.ok) {
+        throw new Error("Somthing went wrong ...Retrying");
+      }
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (e) {
+      setError(e.message);
+      if (isCancel) {
+        retrying = setInterval(fetchMoviesHandler, 5000);
+        arr.push(retrying);
+      }
+    }
+    setisLoader(false);
+  }, []);
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+  function cancleRetryingHanldler() {
+    setisCancel(false);
+    console.log(retrying);
+    arr.map((a) => {
+      console.log(a);
+      clearInterval(a);
+      arr = [];
+    });
+    clearInterval(retrying);
+    setError(null);
+    setisCancel(true);
+  }
+  let content = "no data found";
+  if (isloader) {
+    content = <h1>Loading...</h1>;
+  }
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  if (error) {
+    content = (
+      <div>
+        <p>{error}</p> <button onClick={cancleRetryingHanldler}>Cancel</button>
+      </div>
+    );
+  }
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {/* {isloader && <h1>Loading...</h1>}
+        {!isloader && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isloader && movies.length === 0 && <h1>Found no movies</h1>}
+        {!isloader && error && (
+          <div>
+            <p>{error}</p>{" "}
+            <button onClick={cancleRetryingHanldler}>Cancel</button>
+          </div>
+        )} */}
+        {content}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
