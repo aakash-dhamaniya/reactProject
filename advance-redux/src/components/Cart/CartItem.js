adfafa16a1f30a99b902c8a34621f428626eddd3
aakash-dhamaniya <aakashdhamaniya.dev@gmail.com>
2023-06-18 19:14:01 +0530

    add to cart fix with redux

--- advance-redux/src/components/Cart/CartItem.js
+++ advance-redux/src/components/Cart/CartItem.js
@@ -1,14 +1,25 @@
-import classes from './CartItem.module.css';
+import classes from "./CartItem.module.css";
+import { cartActions } from "../../store/cart-slice";
+import { useDispatch } from "react-redux";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price, id } = props.item;
+  const dispatch = useDispatch();
 
+  const increaseQuantity = () => {
+    dispatch(cartActions.addItemTocart({ title, quantity, total, price, id }));
+  };
+  const decreaseQuantity = () => {
+    dispatch(
+      cartActions.removeItemFromCart({ title, quantity, total, price, id })
+    );
+  };
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
+          ${total.toFixed(2)}{" "}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
@@ -17,8 +28,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={decreaseQuantity}>-</button>
+          <button onClick={increaseQuantity}>+</button>
         </div>
       </div>
     </li>
---
+++ advance-redux/src/store/cart-slice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemTocart(state, action) {
+      console.log(action.payload);
+      const newItem = action.payload;
+      //checking item already exist or not
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload.id;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+export const cartActions = cartSlice.actions;
+export default cartSlice;
