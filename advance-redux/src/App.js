f4b87ce94abaac658596273def5e6c8afad3fd62
aakash-dhamaniya <aakashdhamaniya.dev@gmail.com>
2023-06-20 20:40:45 +0530

    retrive a data from data base and shown to cart

--- advance-redux/src/App.js
+++ advance-redux/src/App.js
@@ -1,58 +1,31 @@
 import { useSelector, useDispatch } from "react-redux";
-import { uiActions } from "./store/ui-slice";
+import { cartActions, getCartData } from "./store/cart-slice";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect } from "react";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
 let initial = true;
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  console.log(cart);
   const notification = useSelector((state) => state.ui.notification);
   const dispatch = useDispatch();
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.shoNotification({
-          status: "pending",
-          title: "sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://expense-tracker-69a2b-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Sending cart data failed");
-      }
-      dispatch(
-        uiActions.shoNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
+    dispatch(getCartData());
+  }, [dispatch]);
+  useEffect(() => {
     if (initial) {
       initial = false;
       return;
     }
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.shoNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    if (cart.change) {
+      dispatch(sendCartData(cart));
+    }
   }, [cart, dispatch]);
-  console.log(notification);
+
   return (
     <>
       {notification && (
--- advance-redux/src/store/cart-slice.js
+++ advance-redux/src/store/cart-slice.js
@@ -1,10 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
     totalQuantity: 0,
+    change: false,
   },
   reducers: {
     replaceCart(state, action) {
@@ -12,6 +14,7 @@ const cartSlice = createSlice({
       state.items = action.payload.items;
     },
     addItemToCart(state, action) {
+      state.change = true;
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
@@ -29,6 +32,7 @@ const cartSlice = createSlice({
       }
     },
     removeItemFromCart(state, action) {
+      state.change = true;
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
@@ -42,6 +46,89 @@ const cartSlice = createSlice({
   },
 });
 
+export const getCartData = () => {
+  console.log("call hua");
+  return async (dispatch) => {
+    const fetchData = async () => {
+      console.log("in fetch data");
+      const response = await fetch(
+        "https://expense-tracker-69a2b-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json"
+      );
+      if (!response.ok) {
+        throw new Error("Could not fetch cart data!");
+      }
+      const data = await response.json();
+      return data;
+    };
+    try {
+      console.log("in try");
+      const cartData = await fetchData();
+      console.log(cartData);
+      if ((cartData.length = !0)) {
+        dispatch(
+          cartActions.replaceCart({
+            items: cartData.items || [],
+            totalQuantity: cartData.totalQuantity,
+          })
+        );
+      }
+    } catch (error) {
+      dispatch(
+        uiActions.shoNotification({
+          status: "error",
+          title: "Error!",
+          message: "Fetching  cart data failed!",
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  console.log("in sendCart data", cart);
+  return async (dispatch) => {
+    dispatch(
+      uiActions.shoNotification({
+        status: "pending",
+        title: "sending...",
+        message: "Sending cart data!",
+      })
+    );
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://expense-tracker-69a2b-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+    };
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.shoNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.shoNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
 export const cartActions = cartSlice.actions;
 
 export default cartSlice;
